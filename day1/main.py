931b760075d0735a1df4d5069d5f3397c40a6334
Shinvalor <hpoon16@huntersoe.org>
2019-12-01 00:37:18 -0500

    Day 1

---
+++ day1/main.py
@@ -0,0 +1,18 @@
+import math
+
+res = 0
+
+filepath = 'input.txt'
+with open(filepath) as fp:
+  line = fp.readline()
+  while line:
+    num = int(line)
+    fuel = math.floor(num/3)-2
+    res += fuel
+    fuel = math.floor(fuel/3)-2
+    while fuel > 0:
+      res += fuel
+      fuel = math.floor(fuel/3)-2
+    line = fp.readline()
+
+print(res)

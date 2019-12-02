d3c09763e2af649196b04a59a09eb821448a89b8
Shinvalor <hpoon16@huntersoe.org>
2019-12-02 04:23:06 -0500

    2015 day 5 finished

--- 2015/day5/main.py
+++ 2015/day5/main.py
@@ -1,18 +1,19 @@
+import re
+
 def checkVowel(s):
-  limit = 0
+  count = 0
   for char in s:
     if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
-      limit += 1
-  return limit >= 3
+      count += 1
+  return count >= 3
 
 def appearTwice(s):
-  for i in range(1,len(s)):
-    if s[i-1] == s[i]:
-      return True
+  return re.search(r'(.)\1',s)
 
 def specialSubString(s):
-  if "ab" in s or "cd" in s or "pq" in s or "xy" in s:
-    return False
+  for i in ["ab", "cd", "pq", "xy"]:
+    if i in s:
+      return False
   return True
 
 nice = 0
@@ -26,17 +27,21 @@ with open(filepath) as fp:
 
 print(nice)
 
+
 # Part Two
 
+def twoPairs(s):
+  return re.search(r'(.)(.).*\1\2',s)
 
-
+def palindrome(s):
+  return re.search(r'(.).\1',s)
 
 nice = 0
-filepath = 'test_part2.txt'
+filepath = 'input.txt'
 with open(filepath) as fp:
   line = fp.readline()
   while line:
-    if checkVowel(line) and appearTwice(line) and specialSubString(line):
+    if twoPairs(line) and palindrome(line):
       nice += 1
     line = fp.readline()
 
